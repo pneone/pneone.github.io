82fc06960601211fc12144c7d46eda1f9cfd3bc8
pneone <94010215+pneone@users.noreply.github.com>
2022-01-17 21:52:48 +0200

    Update script.js

--- Sooninspoon/dist/js/script.js
+++ Sooninspoon/dist/js/script.js
@@ -1,110 +1,18 @@
 $(document).ready(function(){
-    
-    function windowSize(){
-        if ($(window).width() <= '430'){
-            $('.examples_cards').slick({
-                slidesToShow: 1,  
-                arrows: false
-            });
-            $('.reviews_carusel').slick({
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,   
-                autoplay: false,
-                autoplaySpeed: 4000,
-                arrows: false
-            });
-            $('.burger').on('click', function() {
+    $('.burger').on('click', function() {
         
-                $('.burger').toggleClass('burger_active');
-                if($('.burger').hasClass('burger_active')){
-                    $('.content').addClass('content_active');
-                    $('nav').addClass('nav_active');
-                }
-                
-                else{
-                    $('.content').removeClass('content_active');
-                    $('nav').removeClass('nav_active');
-                }
-                
-            });
-        } 
-        else if ($(window).width() <= '1200'){
-            
-            $('.reviews_carusel').slick({
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 1,   
-                autoplay: false,
-                autoplaySpeed: 4000,
-                arrows: false
-            });
-            
-            
-        } 
-        else if ($(window).width() <= '905'){
-            
-            $('.nav_link').on('click', function() {
-                $('.nav').removeClass('nav_active');
-                $('.burger').removeClass('burger_active');
-            });
-            $('.burger').on('click', function() {
-        
-                $('.burger').toggleClass('burger_active');
-                if($('.burger').hasClass('burger_active')){
-                    $('.nav').addClass('nav_active');
-                }
-                
-                else{
-                    $('.nav').removeClass('nav_active');
-                }
-                
-            });
-            
-            
-        } 
-        else{
-            $('.reviews_carusel').slick({
-                infinite: true,
-                slidesToShow: 4,
-                slidesToScroll: 1,   
-                autoplay: true,
-                autoplaySpeed: 4000,
-                arrows: false
-            });
+        $('.burger').toggleClass('burger_active');
+        if($('.burger').hasClass('burger_active')){
+            $('.content').addClass('content_active');
+            $('nav').addClass('nav_active');
         }
-    }
-    $(window).on('load resize',windowSize);
-    
-    
-    $('.questtions_list_item .click').on('click', function() {
         
-        
-        if($(this).hasClass('active')){
-             
-            $(this).removeClass('active');
-        }
         else{
-            $('.questtions_list_item .click').removeClass('active');
-            $(this).addClass('active');
-            
+            $('.content').removeClass('content_active');
+            $('nav').removeClass('nav_active');
         }
         
     });
-    $('.aplication_button').on('click', function() {
-        
-        $('.application_true').addClass('active');
-        if($('.application_true').hasClass('active')){
-            $('.application_true_plus').on('click', function() {
-                $('.application_true').removeClass('active');
-            });
-            
-        }
-        
-        
-    });
     
-    
-    new WOW().init();
 });
 

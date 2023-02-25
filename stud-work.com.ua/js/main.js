a0fa61142fb9ea0e950cedb39cf9ec8396cd8977
Pavel Oberemko <94010215+pneone@users.noreply.github.com>
2023-02-25 20:27:03 +0300

    Add files via upload

---
+++ stud-work.com.ua/js/main.js
@@ -0,0 +1,62 @@
+window.onload = function () {
+    
+    window.setTimeout(function () {
+        document.querySelector('.preloader').style.display = 'none';
+    }, 500);
+  }
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    
+    
+
+    const openBtn = document.querySelector('#open'),
+          closeBtn = document.querySelector('#close')
+          navbarLinks = document.querySelector('.navbar__links');
+
+    openBtn.classList.add('show');
+
+    function openNavbarLinks(){
+        openBtn.classList.toggle('show');
+        closeBtn.classList.toggle('show');
+        navbarLinks.classList.toggle('links-open');
+    }
+
+    openBtn.addEventListener('click', () => {
+        openNavbarLinks();
+    });
+
+    closeBtn.addEventListener('click', () => {
+        openNavbarLinks();
+    });
+
+
+
+
+    //Swiper
+
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".next",
+          prevEl: ".prev",
+        },
+
+        breakpoints: {
+            1024: {
+              slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+              },
+          },
+    });
+
+
+    if(window.innerWidth <= 768){
+        alert('Cool');
+    }
+
+    
+    
+})

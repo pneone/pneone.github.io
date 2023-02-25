5c79fdc8d9fe90738ad189842ba4f8a049ecc628
Pavel Oberemko <94010215+pneone@users.noreply.github.com>
2023-02-25 19:30:27 +0300

    Update main.js

--- stud-work.com.ua/js/main.js
+++ stud-work.com.ua/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
           boxClass:     'wow',      // animated element css class (default is wow)
           animateClass: 'animated', // animation css class (default is animated)
           offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
+          mobile:       false,       // trigger animations on mobile devices (default is true)
           live:         true,       // act on asynchronously loaded content (default is true)
           scrollContainer: null,    // optional scroll container selector, otherwise use window,
           resetAnimation: true,     // reset animation on end (default is true)

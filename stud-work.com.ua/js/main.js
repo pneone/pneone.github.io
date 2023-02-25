59a19ccc4132891f249b7311ce58df1f5b753256
Pavel Oberemko <94010215+pneone@users.noreply.github.com>
2023-02-25 19:29:02 +0300

    Update main.js

--- stud-work.com.ua/js/main.js
+++ stud-work.com.ua/js/main.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    if(window. innerWidth > 768){
-        new WOW().init();
+    let wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       true,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          scrollContainer: null,    // optional scroll container selector, otherwise use window,
+          resetAnimation: true,     // reset animation on end (default is true)
         }
+      );
+      wow.init();
 
     const openBtn = document.querySelector('#open'),
           closeBtn = document.querySelector('#close')
